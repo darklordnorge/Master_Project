37f6417ca7efee0ef173c9065965260ad63d9060
darklordnorge <romteknolog@gmail.com>
2015-08-15 00:18:13 +0100

    Created occupancy_map classes

---
+++ ROB_SENSORS_OBJS/occupancy_map.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stefan on 15/08/15.
+//
+
+#include "occupancy_map.h"
---
+++ ROB_SENSORS_OBJS/occupancy_map.h
@@ -0,0 +1,8 @@
+//
+// Created by stefan on 15/08/15.
+//
+
+#ifndef PROJECT_OCCUPANCY_MAP_H
+#define PROJECT_OCCUPANCY_MAP_H
+
+#endif //PROJECT_OCCUPANCY_MAP_H
